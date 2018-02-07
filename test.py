bbfda4439e54d0356a35911cb2b5e678666c5d68
Yoohogyun <yoohoogun114@naver.com>
2018-02-07 11:37:12 +0900

    remove useless

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
-from socketIO_client import SocketIO, LoggingNamespace
+from socketIO_client_nexus import SocketIO, LoggingNamespace
+import time
+from random import *
 
-with SocketIO('localhost', 3000, LoggingNamespace) as socketIO:
-    socketIO.emit('aaa')
+
+with SocketIO('127.0.0.1', 3000, LoggingNamespace) as socketIO:
+    while True:
+        socketIO.emit('heartbeat',str(randint(60, 120)))
+        time.sleep(1);
     socketIO.wait(seconds=1)
