806bea20d3274c39ee8997407556f9945155a79d
yuhogyun <yoohoogun114@naver.com>
2018-03-01 10:58:08 +0900

    last

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-from socketIO_client_nexus import SocketIO, LoggingNamespace
-import time
-from random import *
-
 # Simple heart beat reader for Raspberry pi using ADS1x15 family of ADCs and a pulse sensor - http://pulsesensor.com/.
 # The code borrows heavily from Tony DiCola's examples of using ADS1x15 with 
 # Raspberry pi and WorldFamousElectronics's code for PulseSensor_Amped_Arduino
@@ -11,15 +7,24 @@ from random import *
 
 import time
 import PCF8591 as ADC
+from socketIO_client_nexus import SocketIO, LoggingNamespace
+socket = None
 
+def connect():
+    global socket
+    socket = SocketIO('13.125.124.80', 3000, LoggingNamespace)
+
+def emit(msg):
+    global socket
+    socket.emit('heartbeat', str(msg))
 
 if __name__ == '__main__':
+    connect()
     ADC.setup(0x48) #TODO: Select the correct ADC channel. I have selected A0 here
     # initializaadction 
     GAIN = 2/3  
     curState = 0
-    threshSetting = 580
-    thresh = threshSetting  # mid point in the waveform
+    thresh = 550  # mid point in the waveform
     P = 512
     T = 512
     stateChanged = 0
@@ -33,87 +38,85 @@ if __name__ == '__main__':
     amp = 100
 
     lastTime = int(time.time()*1000)
-    with SocketIO('13.125.124.80', 3000, LoggingNamespace) as socketIO:
-        # Main loop. use Ctrl-c to stop the code
-        while True:
-            # read from the ADC
-            Signal = int(ADC.read(0))*4
-	    # print "signal: " + str(Signal)
-            ADC.write(0)
-            curTime = int(time.time()*1000)
 
-            sampleCounter += curTime - lastTime;      #                   # keep track of the time in mS with this variable
-            lastTime = curTime
-            N = sampleCounter - lastBeatTime;     #  # monitor the time since the last beat to avoid noise
-            #print N, Signal, curTime, sampleCounter, lastBeatTime
+    # Main loop. use Ctrl-c to stop the code
+    while True:
+		# read from the ADC
+        Signal = int(ADC.read(0))*4
+        ADC.write(0)
+        curTime = int(time.time()*1000)
 
-            ##  find the peak and trough of the pulse wave
-            if Signal < thresh and N > (IBI/5.0)*3.0 :  #       # avoid dichrotic noise by waiting 3/5 of last IBI
-                if Signal < T :                        # T is the trough
-                    T = Signal;                         # keep track of lowest point in pulse wave 
+        sampleCounter += curTime - lastTime;      #                   # keep track of the time in mS with this variable
+        lastTime = curTime
+        N = sampleCounter - lastBeatTime;     #  # monitor the time since the last beat to avoid noise
+        #print N, Signal, curTime, sampleCounter, lastBeatTime
 
-            if Signal > thresh and  Signal > P:           # thresh condition helps avoid noise
-                P = Signal;                             # P is the peak
-                                                    # keep track of highest point in pulse wave
+        ##  find the peak and trough of the pulse wave
+        if Signal < thresh and N > (IBI/5.0)*3.0 :  #       # avoid dichrotic noise by waiting 3/5 of last IBI
+            if Signal < T :                        # T is the trough
+              T = Signal;                         # keep track of lowest point in pulse wave 
 
-            #  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
-            # signal surges up in value every time there is a pulse
-            if N > 250 :                                   # avoid high frequency noise
-                if  (Signal > thresh) and  (Pulse == False) and  (N > (IBI/5.0)*3.0)  :       
-                    Pulse = True;                               # set the Pulse flag when we think there is a pulse
-                    IBI = sampleCounter - lastBeatTime;         # measure time between beats in mS
-                    lastBeatTime = sampleCounter;               # keep track of time for next pulse
+        if Signal > thresh and  Signal > P:           # thresh condition helps avoid noise
+            P = Signal;                             # P is the peak
+                                                # keep track of highest point in pulse wave
 
-                if secondBeat :                        # if this is the second beat, if secondBeat == TRUE
-                    secondBeat = False;                  # clear secondBeat flag
-                    for i in range(0,10):             # seed the running total to get a realisitic BPM at startup
-                        rate[i] = IBI;                      
+          #  NOW IT'S TIME TO LOOK FOR THE HEART BEAT
+          # signal surges up in value every time there is a pulse
+        if N > 100 :                                   # avoid high frequency noise
+            if  (Signal > thresh) and  (Pulse == False) and  (N > (IBI/5.0)*3.0)  :       
+              Pulse = True;                               # set the Pulse flag when we think there is a pulse
+              IBI = sampleCounter - lastBeatTime;         # measure time between beats in mS
+              lastBeatTime = sampleCounter;               # keep track of time for next pulse
 
-                if firstBeat :                        # if it's the first time we found a beat, if firstBeat == TRUE
-                    firstBeat = False;                   # clear firstBeat flag
-                    secondBeat = True;                   # set the second beat flag
-                    continue                              # IBI value is unreliable so discard it
+              if secondBeat :                        # if this is the second beat, if secondBeat == TRUE
+                secondBeat = False;                  # clear secondBeat flag
+                for i in range(0,10):             # seed the running total to get a realisitic BPM at startup
+                  rate[i] = IBI;                      
+
+              if firstBeat :                        # if it's the first time we found a beat, if firstBeat == TRUE
+                firstBeat = False;                   # clear firstBeat flag
+                secondBeat = True;                   # set the second beat flag
+                continue                              # IBI value is unreliable so discard it
 
 
-                # keep a running total of the last 10 IBI values
-                runningTotal = 0;                  # clear the runningTotal variable    
+              # keep a running total of the last 10 IBI values
+              runningTotal = 0;                  # clear the runningTotal variable    
 
-                for i in range(0,9):                # shift data in the rate array
-                    rate[i] = rate[i+1];                  # and drop the oldest IBI value 
-                    runningTotal += rate[i];              # add up the 9 oldest IBI values
+              for i in range(0,9):                # shift data in the rate array
+                rate[i] = rate[i+1];                  # and drop the oldest IBI value 
+                runningTotal += rate[i];              # add up the 9 oldest IBI values
 
-                rate[9] = IBI;                          # add the latest IBI to the rate array
-                runningTotal += rate[9];                # add the latest IBI to runningTotal
-                runningTotal /= 10;                     # average the last 10 IBI values 
-                BPM = 60000/runningTotal;               # how many beats can fit into a minute? that's BPM!
-                if BPM < 150: # remove noise
-                    print 'BPM: {}'.format(BPM)
-                    socketIO.emit('heartbeat',str(BPM)) # Send To Socket IO
-		# else:
-		   # socketIO.emit('heartbeat', str(0))
+              rate[9] = IBI;                          # add the latest IBI to the rate array
+              runningTotal += rate[9];                # add the latest IBI to runningTotal
+              runningTotal /= 10;                     # average the last 10 IBI values 
+              BPM = 60000/runningTotal;               # how many beats can fit into a minute? that's BPM!
+              if BPM < 150 and BPM > 50: # remove noise
+              	print 'BPM: {}'.format(BPM)
+		emit(BPM)
+	      else:
+		thresh = 550
+		P = 512;
+		T = 512;
+		lastBeatTime = sampleCounter;
+		firstBeat = True;
+		secondBeat = False;
+		print "no beats found"
 
-            if Signal < thresh and Pulse == True :   # when the values are going down, the beat is over
-		print "over"
-                Pulse = False;                         # reset the Pulse flag so we can do it again
-                amp = P - T;                           # get amplitude of the pulse wave
-                thresh = amp/2 + T;                    # set thresh at 50% of the amplitude
-                P = thresh;                            # reset these for next time
-                T = thresh;
+        if Signal < thresh and Pulse == True:   # when the values are going down, the beat is over
+            Pulse = False;                         # reset the Pulse flag so we can do it again
+            amp = P - T;                           # get amplitude of the pulse wave
+            thresh = amp/2 + T;                    # set thresh at 50% of the amplitude
+            P = thresh;                            # reset these for next time
+            T = thresh;
 
-            if N > 2500 :                          # if 2.5 seconds go by without a beat
-		print "without"
-                thresh = threshSetting;                          # set thresh default
-                P = 512;                               # set P default
-                T = 512;                               # set T default
-		sampleCounter = 0;
-		lastBeatTime = 0;
-                firstBeat = True;                      # set these to avoid noise
-                secondBeat = False;                    # when we get the heartbeat back
-		Pulse = False;
-		IBI = 600
-		rate = [0]*10
-		amp = 100
-                print "no beats found"
-		socketIO.emit('heartbeat', str(0))
+        if N > 1000 :                          # if 2.5 seconds go by without a beat
+            thresh = 550;                          # set thresh default
+            P = 512;                               # set P default
+            T = 512;                               # set T default
+            lastBeatTime = sampleCounter;          # bring the lastBeatTime up to date        
+            firstBeat = True;                      # set these to avoid noise
+            secondBeat = False;                    # when we get the heartbeat back
+            print "no beats found"
 
-            time.sleep(0.1)
+        time.sleep(0.02)
+
